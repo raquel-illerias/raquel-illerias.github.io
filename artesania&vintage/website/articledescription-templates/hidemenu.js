36e2de5b722bc2c6cb4d5c90d1dfbe07649ff448
Raquel Illerias <raquel.illerias@gmail.com>
2022-06-02 20:11:18 +0100

    nav bar update + blog entry 2

--- artesania&vintage/website/articledescription-templates/hidemenu.js
+++ artesania&vintage/website/articledescription-templates/hidemenu.js
@@ -1,10 +1,25 @@
-const menuIcon = document.querySelector('.burger-menu');
-const myNav = document.querySelector('nav');
+// ================ NAV BAR + LOGO ===============
 
-menuIcon.addEventListener('click', () => {
-    if(myNav.style.display === 'none') {
-        myNav.style.display ='flex';}
-        else {
-            myNav.style.display = 'none';
-        }
-});
+const openMenuButton = document.getElementById('open-menu')
+const closeMenuButton = document.getElementById('close-menu')
+const navMenu = document.getElementById('navigation-menu')
+const homeLogoButton = document.getElementById('home-logo')
+
+function openMenu() {
+  navMenu.style.display = 'flex'
+  navMenu.style.height = `${window.innerHeight}px`
+}
+
+function closeMenu() {
+    navMenu.classList.toggle('close')
+   setTimeout(() => {
+   navMenu.classList.toggle('close')
+    navMenu.style.display = 'none'
+   }, 340)
+
+}
+
+
+openMenuButton.addEventListener('click', openMenu)
+closeMenuButton.addEventListener('click', closeMenu)
+homeLogoButton.addEventListener('click', () => window.scrollTo(0,0))
--- artesania&vintage/website/hidemenu.js
+++ artesania&vintage/website/hidemenu.js
@@ -1,22 +1,42 @@
-const menuIcon = document.querySelector('.burger-menu');
-const myNav = document.querySelector('nav');
+// ================ NAV BAR + LOGO ===============
+const openMenuButton = document.getElementById('open-menu')
+const closeMenuButton = document.getElementById('close-menu')
+const navMenu = document.getElementById('navigation-menu')
+const homeLogoButton = document.getElementById('home-logo')
 
-menuIcon.addEventListener('click', () => {
-    if(myNav.style.display === 'none') {
-        myNav.style.display ='flex';}
-        else {
-            myNav.style.display = 'none';
-        }
-});
+function openMenu() {
+  navMenu.style.display = 'flex'
+  navMenu.style.height = `${window.innerHeight}px`
+}
+
+function closeMenu() {
+    navMenu.classList.toggle('close')
+   setTimeout(() => {
+   navMenu.classList.toggle('close')
+    navMenu.style.display = 'none'
+   }, 340)
+
+}
 
 
-const catIcon = document.querySelector('.dropdown-cat');
-const dropCon = document.querySelector('.dropdown-container');
+openMenuButton.addEventListener('click', openMenu)
+closeMenuButton.addEventListener('click', closeMenu)
+homeLogoButton.addEventListener('click', () => window.scrollTo(0,0))
 
-catIcon.addEventListener('click', () => {
-    if(dropCon.style.display === 'none') {
-        dropCon.style.display ='flex';}
-        else {
-            dropCon.style.display = 'none';
-        }
+// =============== SWIPER API ============
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay :{
+      delay:3500,
+      disableOnInteraction:false,
+  },
+  pagination: {
+      el: '.swiper-pagination',
+      progressbarFillClass: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  }, 
 });
